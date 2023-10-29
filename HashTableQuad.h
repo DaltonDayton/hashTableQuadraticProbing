85eeb67c8d1555bd49a8c3fe787db3b8b952a941
Dalton Dayton <daltondayton1@gmail.com>
2023-10-29 15:25:31 -0600

    Add insert with quadratic probing to Hashtable class

--- HashTableQuad.h
+++ HashTableQuad.h
@@ -10,6 +10,18 @@ private:
 
     // Private helper functions
 
+    /// @brief Calculates the hash index for a given value.
+    /// @param value The integer value for which the hash index needs to be calculated.
+    /// @return The hash index for the input value.
+    int hash(int value);
+
+    /// @brief Computes the index for quadratic probing.
+    /// This function uses the equation H + c1 * i + c2 * i^2 with c1 = 0 and c2 = 1.
+    /// @param H The initial hash index.
+    /// @param i The current probing iteration.
+    /// @return The computed index after applying quadratic probing.
+    int quadraticProbing(int H, int i);
+
 public:
     // Constructors
     Hashtable();
@@ -18,6 +30,9 @@ public:
     ~Hashtable();
 
     // Member functions
+
+    /// @brief Inserts a value into the hash table at its computed hash index.
+    /// @param value The integer value to be inserted into the hash table.
     void insert(const int value);
 };
 
@@ -32,11 +47,29 @@ Hashtable::Hashtable() : currentSize{0}, tableSize{7}, loadFactorThreshold{.65}
 
 Hashtable::~Hashtable()
 {
+    delete[] table;
+}
+
+int Hashtable::hash(int value)
+{
+    return value % tableSize;
+}
+
+int Hashtable::quadraticProbing(int H, int i)
+{
+    return hash(H + (0 * 1) + (1 * (i * i)));
 }
 
 void Hashtable::insert(const int value)
 {
-    int index = value % tableSize;
+    int index = hash(value);
+
+    int i{0};
+    while (table[index] != -1)
+    {
+        index = quadraticProbing(value, i);
+        i++;
+    }
 
     table[index] = value;
 }
--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,12 @@ int main(int argc, char const *argv[])
 {
     Hashtable h1;
     h1.insert(3);
+    h1.insert(10);
+    h1.insert(17);
+    h1.insert(24);
+    h1.insert(31);
+    h1.insert(38);
+    h1.insert(45);
 
     return 0;
 }
